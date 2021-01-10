de9386c36e3335059c6f38ba9970f8374cd96c03
Kaveri07 <73036737+Kaveri07@users.noreply.github.com>
2021-01-10 12:04:33 -0800

    Add files via upload

---
+++ js/Player.js
@@ -0,0 +1,48 @@
+class Player {
+  constructor(){
+    this.index = null;
+    this.distance = 0;
+    this.name = null;
+  }
+
+  getCount(){
+    var playerCountRef = database.ref('playerCount');
+    playerCountRef.on("value",(data)=>{
+      playerCount = data.val();
+    })
+  }
+
+  updateCount(count){
+    database.ref('/').update({
+      playerCount: count
+    });
+  }
+
+  update(){
+    var playerIndex = "players/player" + this.index;
+    database.ref(playerIndex).set({
+      name:this.name,
+      distance:this.distance
+    });
+  }
+
+  getRank() {
+    var rankRef = database.ref("Rank");
+      rankRef.on("value", (data)=> {
+        rank = data.val();
+      })
+  }
+
+  updateRank(r) {
+    database.ref("/").update({
+      Rank: r
+    })
+  }
+
+  static getPlayerInfo(){
+    var playerInfoRef = database.ref('players');
+    playerInfoRef.on("value",(data)=>{
+      allPlayers = data.val();
+    })
+  }
+}
